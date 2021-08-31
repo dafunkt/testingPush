a11bd299b2003588dc8d89c1fd47e2f4ba9b7a5e
sumit saxena <sumit-saxena@hotmail.com>
2021-08-31 13:05:23 +0530

    date-picker-complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {format, addMonths, getUnixTime, fromUnixTime, subMonths} from 'date-fns'
+import {format, addMonths, getUnixTime, fromUnixTime, subMonths, startOfMonth, startOfWeek, endOfMonth, endOfWeek, addDays, eachDayOfInterval, isSameMonth, isSameDay} from 'date-fns'
 import { formatDurationWithOptions } from 'date-fns/fp'
 
 const pickerButton = document.querySelector('.date-picker-button')
@@ -6,12 +6,16 @@ const card = document.querySelector('.date-picker')
 const cardHeader = document.querySelector('.current-month')
 const nextMonthButton = document.querySelector('.next-month-button')
 const previousMonthButton = document.querySelector('.prev-month-button')
+const dateButtons = document.querySelectorAll('.date')
+const dateGrid = document.querySelector('.date-picker-grid-dates')
 let currentDate = new Date()
+
 
 
 pickerButton.addEventListener('click',  () => {
     card.classList.toggle('show');
-    let selectedDate = fromUnixTime(cardHeader.dataset.selected)    
+    let selectedDate = fromUnixTime(cardHeader.dataset.selected)
+    currentDate = selectedDate
     setUpCard(selectedDate)
 })
 
@@ -21,20 +25,54 @@ function setDate(currentDate){
     cardHeader.dataset.selected = getUnixTime(currentDate)
 }
 
-function setUpCard(currentDate){
+function setUpCard(selectedDate){
     cardHeader.innerText = format(currentDate, 'MMMM yyyy')
+    dateGrid.querySelectorAll('*').forEach(n => n.remove())
+    setupDates(selectedDate)
+}
+
+function setupDates(selectedDate) {
+    const firstWeekStart = startOfWeek(startOfMonth(currentDate))
+    const lastWeekStart = endOfWeek(endOfMonth(currentDate))
+
+    const inBetweenDates = eachDayOfInterval({
+        start: firstWeekStart,
+        end: lastWeekStart
+    })
+    inBetweenDates.forEach(date => {
     
+        const dateButton = document.createElement('button');
+        dateButton.classList.add('date')
+        dateButton.innerText = format(date, "d")
+        if(!isSameMonth(date, currentDate)) {
+            dateButton.classList.add('date-picker-other-month-date');
+        }
+
+        if(isSameDay(date, selectedDate)){
+            console.log(selectedDate)
+            dateButton.classList.add('selected')
+        }
+
+        dateButton.addEventListener('click', () => {
+            setDate(date);
+            card.classList.remove('show')
+        })
+        
+        dateGrid.appendChild(dateButton)
+    })
 }
 
 nextMonthButton.addEventListener('click', () => {
+    let selectedDate = fromUnixTime(cardHeader.dataset.selected)
    currentDate = addMonths(currentDate, 1)
-   setUpCard(currentDate);
-   console.log('test')
+   setUpCard(selectedDate);
 })
 previousMonthButton.addEventListener('click', () => {
+    let selectedDate = fromUnixTime(cardHeader.dataset.selected)
     currentDate = subMonths(currentDate, 1)
-    console.log('test')
+    setUpCard(selectedDate);
  })
 
+
+
  setDate(currentDate)
-console.log('test')
